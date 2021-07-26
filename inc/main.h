a9d088e45c939a05ba2190603330cf4cc63a23ba
Terber Matthias (TT-RHC/PAR) <Matthias.Terber@de.bosch.com>
2021-07-26 14:34:23 +0200

    wip

---
+++ inc/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void hello (void);
+void ping (void);
+void pong (void);
+
+#endif // MAIN_H
--- main.c
+++ main.c
@@ -1,34 +1,35 @@
 #include <unistd.h>
 #include <stdio.h>
+#include "main.h"
 #include "blech/control.h"
 
 #define N   1
 
 int main (int argc, char **argv )
 {
-    blc_blech_control_init();
-    blc_blech_control_tick();
+    blc_blech_01control01_init();
+    blc_blech_01control01_tick();
 
     while(1)
     {
         sleep(1);
-        blc_blech_control_tick();
+        blc_blech_01control01_tick();
     }
 
     return 0;
 }
 
-void blc_control_hello (void)
+void hello (void)
 {
     printf("Hello, this is Blech!\n");
 }
 
-void blc_control_ping (void)
+void ping (void)
 {
     printf("PING\n");
 }
 
-void blc_control_pong (void)
+void pong (void)
 {
     printf("PONG\n");
 }
